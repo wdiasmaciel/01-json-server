d4489f43f12bd0e0e4a3cfdf5ce9951446e30db4
wdiasmaciel <wdiasmaciel@gmail.com>
2024-10-24 19:23:29 +0000

    Resolvendo o problema de CORS.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# 02-json-server
+# 01-json-server
+
+1) Instalar o Node.js:
+   https://nodejs.org/en
+
+2) Verificar a versão instalada do Node.js:
+   node -v
+
+3) Atualizar a instalação do npm:
+   npm install -g npm 
+
+4) Verificar a versão instalada do npm:
+   npm -v
+
+5) Instalar o JSON-Server:
+   npm install -g json-server
+
+6) Na raiz do projeto, criar "db/db.json":
+ {
+    "cidades": [
+      {"id": 1, "cidade": "Belo Horizonte", "estado": "MG", "população": 3800000},
+      {"id": 2, "cidade": "São Paulo", "estado": "SP", "população": 11800000},
+      {"id": 3, "cidade": "Rio de Janeiro", "estado": "RJ", "população": 5300000},
+      {"id": 4, "cidade": "Curitiba", "estado": "PR", "população": 1200000},
+      {"id": 5, "cidade": "Fortaleza", "estado": "CE", "população": 2100000}
+    ]
+  }
+ 
+7) Instalar o middleware CORS (Cross-Origin Resource Sharing):
+   npm install cors
+
+8) Executar o arquivo server.js:
+   node server.js
---
+++ app.js
@@ -0,0 +1,33 @@
+/*
+ * Antes de rodar, instalar:
+ * 1) node.js (https://nodejs.org/en)
+ * 2) npm install -g json-server
+ * 3) Na raiz do projeto, criar "db/db.json":
+ {
+    "cidades": [
+      {"id": 1, "cidade": "Belo Horizonte", "estado": "MG", "população": 3800000},
+      {"id": 2, "cidade": "São Paulo", "estado": "SP", "população": 11800000},
+      {"id": 3, "cidade": "Rio de Janeiro", "estado": "RJ", "população": 5300000},
+      {"id": 4, "cidade": "Curitiba", "estado": "PR", "população": 1200000},
+      {"id": 5, "cidade": "Fortaleza", "estado": "CE", "população": 2100000}
+    ]
+  }
+ *  
+ * Para rodar, executar o comando abaixo:
+ * 4) json-server --watch db/db.json --port 3333
+*/
+
+
+const urlCidades = "https://stunning-yodel-wr9wj9jjrrj729996-3333.app.github.dev/cidades"; //'http://localhost:3333/cidades'
+console.log(urlCidades);
+let cidades = []
+
+function carregaDadosJSONServer (funcaoImprimeDadosDoBancoDeDados) {
+    fetch(urlCidades)
+        .then (function (response) { return response.json() })
+        .then (function (dados) {
+            cidades = dados
+            console.log ('Dados carregados!')
+            funcaoImprimeDadosDoBancoDeDados ()
+        })
+}
